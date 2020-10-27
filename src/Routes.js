820e2de776ba3f6862b1e285fe24e66bb0ce8ae2
samuelpauloantonio <samuelpauloantonio1@gmail.com>
2020-10-27 20:41:43 +0100

    configurando o middleware multer para o aploaud de imagens

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const routes = express.Router()
 const productsController = require('./app/controllers/productsControllers')
+const multer = require('./app/middlewares/multer')
+
 
 
 routes.get("/", function(req, res){
@@ -16,9 +18,9 @@ routes.get('/ads/create', (req, res) => {
 routes.get('/products/create', productsController.create)
 routes.get('/products/:id/edit', productsController.edit )
 
-routes.post('/products', productsController.post )
+routes.post('/products', multer.array('photos', 6), productsController.post )
 
-routes.put('/products', productsController.put )
+routes.put('/products', multer.array('photos', 6), productsController.put )
 
 routes.delete('/products', productsController.delete )
 
---
+++ src/app/middlewares/multer.js
@@ -0,0 +1,34 @@
+//require multer
+const multer = require('multer')
+
+//config multer
+const storage = multer.diskStorage({
+
+    destination : (req, file, callback) => {
+
+        callback(null, './public/images')
+    },
+
+
+    filename : (req, file, callback) =>{
+
+        callback(null, `${Date.now().toString()}-${file.originalname}`)
+    }
+})
+
+//images aceites config
+const fileFilter = (req, file, callback)=>{
+    const isAccepted = ['image/png', 'image/jpg', 'image/jpeg']
+    .find(acceptedFormat => acceptedFormat == file.mimetype)
+
+    if(isAccepted){
+        return callback(null, true)
+    }
+
+    return callback(null, false)
+}
+
+module.exports = multer({
+    storage,
+    fileFilter
+})
