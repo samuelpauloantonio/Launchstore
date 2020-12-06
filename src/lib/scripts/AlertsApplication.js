8606e736437ba174623a1d0a225c13bc2010f848
samuelpauloantonio <samuelpauloantonio1@gmail.com>
2020-12-06 13:06:30 +0100

    menu de usuario

--- src/lib/scripts/AlertsApplication.js
+++ src/lib/scripts/AlertsApplication.js
@@ -4,27 +4,35 @@ const currentpage = document.location.pathname;
 const pathsNames = ['edit', 'users']
 
 
-const form = document.querySelector('#form-delete')
-
-form.addEventListener('click', (event) => {
 
 
-  for (let i = 0; i < pathsNames.length; i++) {
-    if (currentpage.includes(pathsNames[i])) {
+for (let i = 0; i < pathsNames.length; i++) {
+  if (currentpage.includes(pathsNames[i])) {
 
-      const message = form.dataset.message
-
-      const confirmAlert = confirm(message)
+    if(document.querySelector('#form-delete')){
 
 
-      if (!confirmAlert) {
-        event.preventDefault()
+      const form = document.querySelector('#form-delete')
 
-      }
+      form.addEventListener('click', (event) => {
+
+          const message = form.dataset.message
+    
+          const confirmAlert = confirm(message)
+    
+    
+          if (!confirmAlert) {
+            event.preventDefault()
+          }
+       
+    })
     }
+    
   }
+}
 
-})
+
+
 
 
 
