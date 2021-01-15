e93701bd748bd9c226dfe43ef6afeb985c024f68
samuelpauloantonio <samuelpauloantonio1@gmail.com>
2021-01-15 07:20:38 +0100

    umbler

--- src/config/BD_conection.js
+++ src/config/BD_conection.js
@@ -1,8 +1,8 @@
 const { Pool } = require('pg')
 
 module.exports = new Pool({
-  name:"gitpod",
-  password:"gitpod123",
+  name:"samuel1997spa@gmail.com",
+  password:"51053455",
   host:"postgresql-18318-0.cloudclusters.net",
   port:18318,
   database:"launchstoreDB" 
