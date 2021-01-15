caffdf63326639dd7f1061d5d17b51976038432f
samuelpauloantonio <samuelpauloantonio1@gmail.com>
2021-01-15 07:47:26 +0100

    criando nova conexao com o DB

--- src/config/BD_conection.js
+++ src/config/BD_conection.js
@@ -2,9 +2,9 @@ const { Pool } = require('pg')
 
 module.exports = new Pool({
   name:"spa",
-  password:"51053455",
+  password:51053455,
   host:"localhost",
-  port:5434,
-  database:"launchstoreDB" 
+  port:5432,
+  database:"launchstoredb"
 
 }) 
