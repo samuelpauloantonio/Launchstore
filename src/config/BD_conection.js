b930f2d41f77a94623bd644acbb4606d170fe5b7
samuelpauloantonio <samuelpauloantonio1@gmail.com>
2021-01-15 07:32:38 +0100

    ref

--- src/config/BD_conection.js
+++ src/config/BD_conection.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg')
 
 module.exports = new Pool({
-  name:"samuel1997spa@gmail.com",
+  name:"spa",
   password:"51053455",
   host:"postgresql-18318-0.cloudclusters.net",
   port:18318,
--- src/server.js
+++ src/server.js
@@ -37,7 +37,7 @@ nunjucks.configure('src/app/views', {
 })
 
 
-server.listen(3000, function(){
+server.listen(2014, function(){
   console.log("servidor is running")
 })
  
