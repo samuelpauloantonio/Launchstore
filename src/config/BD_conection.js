e88cbf69a1afac5a914fd9c50fb85951cbc38506
samuelpauloantonio <samuelpauloantonio1@gmail.com>
2021-01-15 07:44:21 +0100

    mudando as portas do banco de dados

--- src/config/BD_conection.js
+++ src/config/BD_conection.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg')
 module.exports = new Pool({
   name:"spa",
   password:"51053455",
-  host:"locahost",
+  host:"localhost",
   port:5434,
   database:"launchstoreDB" 
 
